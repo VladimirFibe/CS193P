e7b15812a6fa100043b40d48527f0ae6dbcbc8e3
Vladimir Fibe <motiw@icloud.com>
2023-06-21 06:41:54 +0600

    1.0:30

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("👻")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
