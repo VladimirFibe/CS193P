94d34b4745a405c369e736bf31ffe6afd09127bf
Vladimir Fibe <motiw@icloud.com>
2023-06-23 06:54:38 +0600

    2. 52:58

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -1,12 +1,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚕", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚜", "🚲", "🛵", "🛺", "🚔", "🚍", "🚖", "🚃", "🚋", "🚂", "✈️", "🚀", "🛸", "🚁", "🛶", "⛵️", "🚤", "🛳️", "🚈", "🚊", "🚠"]
+    @State var emojiCount = 3
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("👻")
-                .font(.largeTitle)
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            HStack {
+                Button {
+                    emojiCount -= 1
+                } label: {
+                    Image(systemName: "minus.circle")
+                        .imageScale(.large)
+                }
+                Spacer()
+                Button {
+                    emojiCount += 1
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .imageScale(.large)
+                }
+            }
         }
         .foregroundColor(.red)
         .padding()
@@ -15,4 +33,24 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill(Color.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
 }
