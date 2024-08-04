62b34e11c3cfc4f75aad3ddb89655f1372228b45
Vladimir Fibe <motiw@icloud.com>
2024-08-04 16:20:30 +0500

    lecture1

--- Memorize/Memorize/GameView.swift
+++ Memorize/Memorize/GameView.swift
@@ -4,9 +4,9 @@ struct GameView: View {
     var body: some View {
         HStack {
             CardView()
+            CardView(isFaceUp: false)
             CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: false)
         }
         .foregroundStyle(.orange)
         .padding()
@@ -14,13 +14,17 @@ struct GameView: View {
 }
 
 struct CardView: View {
+    var isFaceUp: Bool = true
     var body: some View {
+        let shape = RoundedRectangle(cornerRadius: 12)
         ZStack {
-            RoundedRectangle(cornerRadius: 12)
-                .foregroundStyle(.white)
-            RoundedRectangle(cornerRadius: 12)
-                .strokeBorder(lineWidth: 2)
-            Text("🎃").font(.largeTitle)
+            if isFaceUp {
+                shape.foregroundStyle(.white)
+                shape.strokeBorder(lineWidth: 2)
+                Text("🎃").font(.largeTitle)
+            } else {
+                shape
+            }
         }
     }
 }
